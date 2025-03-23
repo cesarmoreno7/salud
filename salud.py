a4e1deef7f152e29a982cf77028229c4d6f3a596
cesarmoreno7 <cesarmoreno7@gmail.com>
2025-03-22 19:28:23 -0500

    Update salud.py

--- salud.py
+++ salud.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 # Cargar datos
 def load_data():
